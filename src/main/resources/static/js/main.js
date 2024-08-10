90cbc0d038114cf1f70366052f77a6f95d63b13c
Layyyth <111781588+Layyyth@users.noreply.github.com>
2024-08-10 21:21:02 +0300

    Add files via upload

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -20,7 +20,7 @@ function connect(event){
         usernamepage.classList.add('hidden');
         chatPage.classList.remove('hidden');
 
-        var socket = new SockJS('https://aqueous-escarpment-60435-7d9c94ee0934.herokuapp.com/ws');
+        var socket = new SockJS('https://echannelz-demo-chatapp-1c44974fe343.herokuapp.com/ws');
 
 
         stompClient = Stomp.over(socket);
